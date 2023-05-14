68eac977d22ee7e410750571471fa52b26017ce8
Bellzmm <1124114812@qq.com>
2023-05-14 23:24:25 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。",
+      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。需要更多服务添加微信：nclzhm进行咨询！",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
@@ -168,7 +168,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗？需要更多服务添加微信：nclzhm，咨询！",
+    BotHello: "有什么可以帮你的吗？需要更多服务添加微信：nclzhm进行咨询！",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) =>
